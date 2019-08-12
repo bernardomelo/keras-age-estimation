9d1ec7512651bf4f7a9b7b5a5dc6e55c5b9619d0
Bernardo Gomes de Melo <bgm@cin.ufpe.br>
2019-08-12 04:12:29 -0300

    update file

--- adaboost.py
+++ adaboost.py
@@ -11,22 +11,20 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 eyeCascade = cv2.CascadeClassifier(eyePath)
 smileCascade = cv2.CascadeClassifier(smilePath)
 
-# Load the image
+# Load image
 gray = cv2.imread('face_detect_test.jpeg', 0)
 #plt.figure(figsize=(12,8))
 #plt.imshow(gray, cmap='gray')
 #plt.show()
 
-# Detect faces
 faces = faceCascade.detectMultiScale(
 gray,
 scaleFactor=1.1,
 minNeighbors=5,
 flags=cv2.CASCADE_SCALE_IMAGE
 )
-# For each face
+
 for (x, y, w, h) in faces:
     # Draw rectangle around the face
     cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 255, 255), 3)
 
-#
