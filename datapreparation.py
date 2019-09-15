d972b8d7519a0961ec3191ef62e47eba6244ce2a
Bernardo Gomes de Melo <bgm@cin.ufpe.br>
2019-09-15 06:40:37 -0300

    updating
    .

--- datapreparation.py
+++ datapreparation.py
@@ -1,5 +1,22 @@
+from keras.callbacks import ModelCheckpoint
 from keras.utils import to_categorical
 from PIL import Image
+
+
+p = np.random.permutation(len(df))
+train_up_to = int(len(df) * TRAIN_TEST_SPLIT)
+train_idx = p[:train_up_to]
+test_idx = p[train_up_to:]
+
+#split train em train e validacao
+train_up_to = int(train_up_to * 0.7)
+train_idx, valid_idx = train_idx[:train_up_to], train_idx[train_up_to:]
+
+df['gender_id'] = df['gender'].map(lambda gender: GENDER_ID_MAP[gender])
+df['race_id'] = df['race'].map(lambda race: RACE_ID_MAP[race])
+
+max_age = df['age'].max()
+len(train_idx), len(valid_idx), len(test_idx), max_age
 
 
 def get_data_generator(df, indices, for_training, batch_size=16):
@@ -21,3 +38,43 @@ def get_data_generator(df, indices, for_training, batch_size=16):
             break
 
     yield np.array(images), [np.array(ages), np.array(genders)]
+
+
+def plot_train_history(history):
+    fig, axes = plt.subplots(1, 4, figsize=(20, 5))
+    axes[0].plot(history.history['race_output_acc'], label='Race Train accuracy')
+    axes[0].plot(history.history['val_race_output_acc'], label='Race Val accuracy')
+    axes[0].set_xlabel('Epochs')
+    axes[0].legend()
+
+    axes[1].plot(history.history['gender_output_acc'], label='Gender Train accuracy')
+    axes[1].plot(history.history['val_gender_output_acc'], label='Gener Val accuracy')
+    axes[1].set_xlabel('Epochs')
+    axes[1].legend()
+
+    axes[2].plot(history.history['age_output_mean_absolute_error'], label='Age Train MAE')
+    axes[2].plot(history.history['val_age_output_mean_absolute_error'], label='Age Val MAE')
+    axes[2].set_xlabel('Epochs')
+    axes[2].legend()
+
+    axes[3].plot(history.history['loss'], label='Training loss')
+    axes[3].plot(history.history['val_loss'], label='Validation loss')
+    axes[3].set_xlabel('Epochs')
+    axes[3].legend()
+
+
+
+#treinando abaixo
+batch_size = 64
+valid_batch_size = 64
+train_gen = get_data_generator(df, train_idx, for_training=True, batch_size=batch_size)
+valid_gen = get_data_generator(df, valid_idx, for_training=True, batch_size=valid_batch_size)
+
+callbacks = [ModelCheckpoint("./model_checkpoint", monitor='val_loss')]
+history = model.fit_generator(train_gen,
+                    steps_per_epoch=len(train_idx)//batch_size,
+                    epochs=10,
+                    callbacks=callbacks,
+                    validation_steps=len(valid_idx)//valid_batch_size)
+
+#plot_train_history(history)
